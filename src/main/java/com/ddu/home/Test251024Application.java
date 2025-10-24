a5f989710cf94470343356712216e15836014fec
ddujeong <qoddl159@nate.com>
2025-10-24 11:39:03 +0900

    기본 프로젝트 설정

---
+++ src/main/java/com/ddu/home/Test251024Application.java
@@ -0,0 +1,13 @@
+package com.ddu.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test251024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test251024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddu/home/Test251024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddu.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test251024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
